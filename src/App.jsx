69b42be6d0e36c4cef7e73804e3ecf9219e8da0b
XPM56780 <xp.marechal@free.fr>
2024-12-13 10:23:52 +0100

    second-commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import PokemonCard from "./components/PokemonCard";
+
+function App() {
+  return (
+    <section>
+      <h1>Hello</h1>
+      <div><PokemonCard /></div>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function PokemonCard() {
+    return (
+        <div>
+            <figure>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+
+            </figure>
+            <figcaption>Bulbasaur</figcaption>
+        </div>
+    )
+}
+
+export default PokemonCard;
